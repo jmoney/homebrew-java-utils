9fe1a1cf2f19d974784e4349fd8cd4814fd5b04f
Jonathan Monette <jon.monette@gmail.com>
2019-02-18 13:38:20 -0600

    Remove undent

--- saxoncli.rb
+++ saxoncli.rb
@@ -15,7 +15,7 @@ class Saxoncli < Formula
     lib.install "lib/Saxon-HE-9.4.0.7.jar"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<-EOS
     You need at least java 1.6 for this to work.  Homebrew does not have a
     java installation support(that I could find) so could not have a depends_on for it.
     EOS
